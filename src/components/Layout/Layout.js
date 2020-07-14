44a695142f8930ddc7b0ea887da9e32b85a1dbe2
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-07-13 23:51:21 -0400

    create BurgerBuilder component and Layout CSS file

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import LayoutStyles from './Layout.module.css';
 
 const Layout = props => {
   return (
     <Aux>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>
+      <main className={LayoutStyles.content}>
         {props.children}
       </main>
     </Aux>
