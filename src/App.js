02f03215009def513c500e2fc49bba1ebc59a6c6
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-08-16 21:28:52 -0400

    add route guards to ensure user authentication

--- src/App.js
+++ src/App.js
@@ -20,21 +20,40 @@ class App extends Component {
   }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" component={BurgerBuilder}/>
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/logout" component={Logout}/>
+          <Route path="/orders" component={Orders}/>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/auth" component={Auth}/>
+          <Route path="/" component={BurgerBuilder}/>
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/logout" component={Logout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/auth" component={Auth}/>
-            <Route path="/" component={BurgerBuilder}/>
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -42,4 +61,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
